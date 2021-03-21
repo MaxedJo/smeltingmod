a62c4e6329f4164bcf1909be02b5ca9e771b0da4
Maxed <maxim.a.vorobyev@gmail.com>
2021-03-21 13:23:18 +0300

    Added blacklist and debugMode congigs New Config class and ore blacklist

--- src/main/java/ru/maxed/smelting/Smelting.java
+++ src/main/java/ru/maxed/smelting/Smelting.java
@@ -1,14 +1,10 @@
 package ru.maxed.smelting;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.Config.Comment;
-import net.minecraftforge.common.config.Config.RangeInt;
-import net.minecraftforge.common.config.Config.Type;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid= Smelting.MODID, name= Smelting.MODNAME, version = Smelting.VERSION)
+@Mod(modid= Smelting.MODID, name= Smelting.MODNAME, version = Smelting.VERSION,acceptableRemoteVersions = "*")
 public class Smelting {
 
     public static final String MODID = "smelting";
@@ -19,25 +15,6 @@ public class Smelting {
     public void load(FMLInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(new oreBreakIvent());
-    }
-
-
-    @Config(modid = MODID, type = Type.INSTANCE, name = MODID)
-    public static class Configs{
-        @Comment({
-                "How many exp you will get for one block[DEFAULT=3]"
-        })
-        public static int expFromBlock=3;
-        @Comment({
-                "Chance(%) of good Luck (fortune drop + 1 )[DEFAULT=10]"
-        })
-        @RangeInt(min = 0,max = 100)
-        public static int goodLuck=10;
-        @Comment({
-                "Chance(%) of bad Luck (fortune drop - 1 )[DEFAULT=20]"
-        })
-        @RangeInt(min = 0,max = 100)
-        public static int badLuck=20;
     }
 
 }
--- src/main/java/ru/maxed/smelting/oreBreakIvent.java
+++ src/main/java/ru/maxed/smelting/oreBreakIvent.java
@@ -4,12 +4,15 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.init.Enchantments;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
 
@@ -18,19 +21,21 @@ public class oreBreakIvent {
     public int quantityDropped(int fortune, Random random) {
         int i=fortune;
         int luck=random.nextInt(100) ;
-        if (luck >= 100-Smelting.Configs.goodLuck)  i++;
-         else if (luck <= Smelting.Configs.badLuck)  i--;
+        if (luck >= 100-Configs.goodLuck)  i++;
+         else if (luck <= Configs.badLuck)  i--;
         return (i > 0 ) ? i : 1;
     }
 
     @SubscribeEvent(priority = EventPriority.LOW)
     public void dropSmelting(BlockEvent.HarvestDropsEvent event) {
         Random rand = new Random();
-        if(event.getHarvester() != null && !event.isSilkTouching() && event.getState().getBlock().getUnlocalizedName().toLowerCase().contains("ore")) {
+        if (Configs.debugMode && event.getHarvester() != null) event.getHarvester().sendMessage(new TextComponentString(event.getState().getBlock().getRegistryName().toString()));
+        boolean check =   !Arrays.asList(Configs.oreBlackList).contains(event.getState().getBlock().getRegistryName().toString());
+        if(check && event.getHarvester() != null && !event.isSilkTouching() && event.getState().getBlock().getUnlocalizedName().toLowerCase().contains("ore")) {
             ItemStack main = event.getHarvester().getHeldItemMainhand();
             if(!main.isEmpty()) {
                     int fortuneLvl = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, main);
-                    NonNullList<ItemStack> newStacks = NonNullList.create();
+                        NonNullList<ItemStack> newStacks = NonNullList.create();
                     Iterator<ItemStack> iterator = event.getDrops().iterator();
                     while (iterator.hasNext()) {
                         ItemStack stack = iterator.next();
@@ -54,8 +59,9 @@ public class oreBreakIvent {
     @SubscribeEvent
     public void handleExpDrops(BlockEvent.BreakEvent event){
         boolean canHarvest = ForgeHooks.canHarvestBlock(event.getState().getBlock(), event.getPlayer(), event.getWorld(), event.getPos());
-        if(canHarvest  && event.getPlayer() != null && !(EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, event.getPlayer().getHeldItemMainhand()) > 0) && event.getState().getBlock().getUnlocalizedName().toLowerCase().contains("ore")){
-            event.setExpToDrop(Smelting.Configs.expFromBlock);
+        boolean check =   !Arrays.asList(Configs.oreBlackList).contains(event.getState().getBlock().getRegistryName().toString());
+        if(check && canHarvest  && event.getPlayer() != null && !(EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, event.getPlayer().getHeldItemMainhand()) > 0) && event.getState().getBlock().getUnlocalizedName().toLowerCase().contains("ore")){
+                event.setExpToDrop(Configs.expFromBlock);
         }
     }
 }
